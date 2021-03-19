630d527cb132f2ace0820b690d9ca945ae0ee343
Marco Antônio <marcomrj@outlook.com>
2021-03-19 03:50:56 -0300

    Crianção das connexões

--- backend/src/database/routes.js
+++ backend/src/database/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const crypto = require('crypto');
 const routes = express.Router();
+const connection = require('./database/connection');
 
-routes.post('/users', (request,response)=>{
-    return response.json({
-        evento: 'Semana Omnistack 22.0',
-        aluno: 'Marco'
-    });
+routes.post('/ongs', (request,response)=>{
+    const {name, email, whatsapp, city, uf} = request.body;
+    
+    const id = crypto.randomBytes(4).toString('hex');
+    
+    return response.json();
 });
 
 module.exports = routes;
