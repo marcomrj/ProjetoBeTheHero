e4545178305469c446ba3ac0b9cbe65b5ab85259
Marco Antônio <marcomrj@outlook.com>
2021-03-29 22:51:02 -0300

    Inclusão de comentários

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,14 +6,14 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 
-routes.post('/session', SessionController.create);
+routes.post('/session', SessionController.create);  //Rota que gerencia a criação de contas
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs',OngController.create);        //Função que recebe os valores colocados no post nas variáveis definidas
 
-routes.get('/incidents',IncidentController.index);
-routes.post('/incidents',IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents',IncidentController.index);  //Rota que gerencia a listagem de incidentes
+routes.post('/incidents',IncidentController.create);   //Rota que gerencia criação de incidentes
+routes.delete('/incidents/:id', IncidentController.delete); //Rota que genrencia a deletação de incidentes
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', ProfileController.index);    //Rota que gerencia a listagem de perfis
 module.exports = routes;
