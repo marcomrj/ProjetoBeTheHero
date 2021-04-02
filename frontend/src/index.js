bec1a402d4183f0b1b83bed592997627110c6d23
Marco Antônio <marcomrj@outlook.com>
2021-04-02 17:23:07 -0300

    Base do frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';      //Importa o react para a página
+import ReactDOM from 'react-dom';      
+import App from './App';        //Importa a página App
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
