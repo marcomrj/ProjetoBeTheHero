cdb2caf2d999d9dbaedf7fa963f5d94fa7b736d2
Marco Antônio <marcomrj@outlook.com>
2021-04-03 23:56:04 -0300

    Comentarios e resetando CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';      //Importa o react para a página
-import ReactDOM from 'react-dom';      
+import ReactDOM from 'react-dom';      //Importa chamada de renderização
 import App from './App';        //Importa a página App
 
 
-ReactDOM.render(
+ReactDOM.render(        //Função que renderiza em tela o conteúdo nela chamado
   <React.StrictMode>
-    <App />
+    <App />             
   </React.StrictMode>,
   document.getElementById('root')
 );
