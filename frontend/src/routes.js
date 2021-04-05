71131601c3032ef563fed616db4afd21f8c7e930
Marco Antônio <marcomrj@outlook.com>
2021-04-05 18:01:08 -0300

    Front página de novos casos

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
 export default function Routes(){
     return(
         <BrowserRouter>
@@ -11,6 +13,7 @@ export default function Routes(){
             <Route path="/" exact component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/profile" component={Profile} />
+            <Route path="/incidents/new" component={NewIncident} />
         </Switch>
         </BrowserRouter>
     );
